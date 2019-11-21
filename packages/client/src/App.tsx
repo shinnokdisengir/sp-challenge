814835f1a23755368447918cb84d80ae34b7489c
Gabriele Giovanni Celso Ghio <gabriele.ghio@wyscout.com>
2019-11-22 00:48:13 +0100

    [COMPLETED] app completed

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, HTMLProps } from 'react'
+import Background from './Background'
 import PokeTable from './PokeTable'
 import styled from 'styled-components'
 
@@ -6,13 +7,41 @@ interface Props extends HTMLProps<HTMLDivElement> {}
 const App: FunctionComponent<Props> = ({ className }) => {
     return (
         <div className={className}>
-            <PokeTable className='table' />
+            <Background className='background' />
+            <div className='table-container'>
+                <PokeTable className='table' />
+            </div>
         </div>
     )
 }
 const StyledApp = styled(App)`
-    padding: 16px;
-    .table {
+    position: relative;
+    background-color: #03a9f4;
+
+    &,
+    .background {
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+    }
+
+    .background {
+        position: absolute;
+    }
+    .table-container {
+        position: relative;
+        display: flex;
+        width: 50%;
+        height: 640px;
+        left: 25%;
+        top: calc(50% - 320px);
+        border: 4px solid #03a9f4;
+        background-color: white;
+        padding-bottom: 32px;
+        .table {
+            flex: 1;
+        }
     }
 `
 export default StyledApp
--- packages/client/src/PokeTable.tsx
+++ packages/client/src/PokeTable.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/react-hooks'
 import { Button, Input, Table, Tag, Typography, Row, Col } from 'antd'
 import { PaginationConfig } from 'antd/lib/pagination'
+import Scroller from 'react-infinite-scroller'
 import {
     SorterResult,
     TableCurrentDataSource,
@@ -27,15 +28,20 @@ const mapColorType = {
 }
 
 const POKEMONS = gql`
-    query($name: String, $types: [String]) {
+    query($name: String, $types: [String], $limit: Int, $after: ID) {
         pokemonsTypes
-        pokemons(q: $name, types: $types) {
+        pokemons(q: $name, types: $types, after: $after, limit: $limit) {
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
             edges {
                 node {
                     id
                     name
                     url
                     types
+                    classification
                 }
             }
         }
@@ -50,37 +56,46 @@ type OnTableChange<T> = (
     extra: TableCurrentDataSource<T>
 ) => void
 
-const PokeTable = forwardRef<Table<Pokemon>, Props>(
+const limit = 50
+const PokeTable = forwardRef<HTMLDivElement, Props>(
     ({ className, ...props }, ref) => {
         const [name, setName] = useState<string | undefined>()
         const [types, setTypes] = useState<string[]>([])
+        const [after, setAfter] = useState<string | undefined>()
         const [, setFilterOpened] = useState<boolean>(false)
+        const [dataSource, setDataSource] = useState<Pokemon[]>([])
 
         const { loading, error, data } = useQuery(POKEMONS, {
-            variables: { name, types }
+            variables: { name, types, limit, after },
+            onCompleted: d => {
+                setDataSource(
+                    dataSource.concat(d.pokemons.edges.map(e => e.node))
+                )
+            }
         })
 
         const handleChange: OnTableChange<Pokemon> = useCallback(
-            (pagination, filters, sorter, extra) => {
-                if (filters.name) setName(filters.name[0])
-                if (filters.types) setTypes(filters.types)
-            },
+            (_pagination, filters) => (
+                setDataSource([]),
+                filters.name && setName(filters.name[0]),
+                filters.types && setTypes(filters.types),
+                setAfter(undefined)
+            ),
             []
         )
 
         const handleSearch = useCallback(
-            ({ target: { value } }) => setName(value),
+            ({ target: { value } }) => (setDataSource([]), setName(value)),
             []
         )
 
         const renderSearch = useCallback(
             ({
                 setSelectedKeys,
-                selectedKeys,
+                selectedKeys: _,
                 confirm,
                 clearFilters
             }: FilterDropdownProps) => {
-                console.log('setS :', selectedKeys)
                 return (
                     // Is out of DOM PokeTable's ownership
                     <Row type='flex' gutter={8} style={{ padding: 8 }}>
@@ -117,66 +132,75 @@ const PokeTable = forwardRef<Table<Pokemon>, Props>(
 
         if (error) return <p>Error :(</p>
 
-        console.log('data', data)
-
-        const dataSource: Array<Pokemon> = !loading
-            ? data.pokemons.edges.map(e => e.node)
-            : []
-
         return (
-            <Table
-                {...props}
-                ref={ref}
-                className={className}
-                loading={loading}
-                dataSource={dataSource}
-                rowKey='id'
-                onChange={handleChange}
-            >
-                <Column<Pokemon>
-                    title='Name'
-                    key='name'
-                    dataIndex='name'
-                    ellipsis
-                    filterDropdown={renderSearch}
-                    onFilterDropdownVisibleChange={setFilterOpened}
-                    render={name => <Text strong>{name}</Text>}
-                />
-                <Column<Pokemon>
-                    title='Types'
-                    key='types'
-                    dataIndex='types'
-                    filters={
-                        data &&
-                        data.pokemonsTypes.map(t => ({
-                            text: t,
-                            value: t
-                        }))
-                    }
-                    filterMultiple
-                    render={types => (
-                        <>
-                            {types.map(t => (
-                                <Tag
-                                    color={mapColorType[t.toLowerCase()]}
-                                    key={t}
-                                >
-                                    {t}
-                                </Tag>
-                            ))}
-                        </>
-                    )}
-                />
-                <Column<Pokemon>
-                    title='Classification'
-                    key='classification'
-                    dataIndex='classification'
-                    ellipsis
-                    render={classification => classification}
-                />
-            </Table>
+            <div ref={ref} className={className}>
+                <Scroller
+                    initialLoad
+                    pageStart={0}
+                    loadMore={() => {
+                        setAfter(data.pokemons.pageInfo.endCursor)
+                    }}
+                    hasMore={!loading && data.pokemons.pageInfo.hasNextPage}
+                    useWindow={false}
+                >
+                    <Table
+                        {...props}
+                        loading={loading}
+                        dataSource={dataSource}
+                        rowKey='id'
+                        onChange={handleChange}
+                        pagination={false}
+                    >
+                        <Column<Pokemon>
+                            title='Name'
+                            key='name'
+                            dataIndex='name'
+                            ellipsis
+                            filterDropdown={renderSearch}
+                            onFilterDropdownVisibleChange={setFilterOpened}
+                            render={name => <Text strong>{name}</Text>}
+                        />
+                        <Column<Pokemon>
+                            title='Types'
+                            key='types'
+                            dataIndex='types'
+                            filters={
+                                data &&
+                                data.pokemonsTypes.map(t => ({
+                                    text: t,
+                                    value: t
+                                }))
+                            }
+                            filterMultiple
+                            render={types => (
+                                <>
+                                    {types.map(t => (
+                                        <Tag
+                                            color={
+                                                mapColorType[t.toLowerCase()]
+                                            }
+                                            key={t}
+                                        >
+                                            {t}
+                                        </Tag>
+                                    ))}
+                                </>
+                            )}
+                        />
+                        <Column<Pokemon>
+                            title='Classification'
+                            key='classification'
+                            dataIndex='classification'
+                            ellipsis
+                            render={classification => classification}
+                        />
+                    </Table>
+                </Scroller>
+            </div>
         )
     }
 )
-const StyledPokeTable = styled(PokeTable)``
+const StyledPokeTable = styled(PokeTable)`
+    overflow-y: auto;
+`
 export default StyledPokeTable
